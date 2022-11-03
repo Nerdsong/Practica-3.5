49dbc0ceecd9a61939b0aa7e0e0c33bb3a6844b3
Nelson Adarmes <jhoeladarmes@gmail.com>
2022-11-03 11:24:24 -0300

    corregi imagenes

--- listados/listadoGorras.js
+++ listados/listadoGorras.js
@@ -3,17 +3,17 @@ import {Gorra} from "../classesdeElementosBasicos/Gorra.js"
 let listadoGorras = [
     new Gorra ("Negro", 
     "Gorra ajustable Logo MOOG", 
-    "imagenes/imagenesModelos/Moog Gorra Negra.jpg"
+    "./imagenes/imagenesModelos/Moog Gorra Negra.jpg"
     ),
 
     new Gorra ("Blanco", 
     "Gorra ajustable Synth", 
-    "imagenes/imagenesModelos/Moog Gorra Blanca.jpg"
+    "./imagenes/imagenesModelos/Moog Gorra Blanca.jpg"
     ),
 
     new Gorra ("Azul", 
     "Gorra ajustable Star", 
-    "imagenes/imagenesModelos/Moog Gorra Azul.jpg"
+    "./imagenes/imagenesModelos/Moog Gorra Azul.jpg"
     ),
 ]
 
--- listados/listadoSintetizadores.js
+++ listados/listadoSintetizadores.js
@@ -4,20 +4,21 @@ let listadoSintetizador = [
     new Sintetizador (
         "Grandmother",
         "2018",
-        "imagenes/imagenesModelos/Moog Grandmother.jpg"
+        "./imagenes/imagenesModelos/Moog Grandmother.jpg"
     ),
 
     new Sintetizador (
         "Model D", 
         "1970",
-        "imagenes/imagenesModelos/Moog model d.jpg"
+        "./imagenes/imagenesModelos/Moog model d.jpg"
     ),
 
     new Sintetizador (
         "Subsecuent37", 
         "2014",
-        "imagenes/imagenesModelos/Moog subsecuent 37.jpg"
+        "./imagenes/imagenesModelos/Moog subsecuent 37.jpg"
     )
+    
 ]
 
 
