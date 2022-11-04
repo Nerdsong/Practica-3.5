45d5ea9e8f5dd768faa7fbe07a107b2132e295d3
Nelson Adarmes <jhoeladarmes@gmail.com>
2022-11-04 09:17:05 -0300

    corrección de nombres en archivos foto

--- listados/listadoSintetizadores.js
+++ listados/listadoSintetizadores.js
@@ -4,19 +4,19 @@ let listadoSintetizador = [
     new Sintetizador (
         "Grandmother",
         "2018",
-        "../imagenes/imagenesModelos/Moog Grandmother.jpg"
+        "../imagenes/imagenesModelos/Moog_Grandmother.jpg"
     ),
 
     new Sintetizador (
         "Model D", 
         "1970",
-        "../imagenes/imagenesModelos/Moog model d.jpg"
+        "../imagenes/imagenesModelos/Moog_Model_D.jpg"
     ),
 
     new Sintetizador (
         "Subsecuent37", 
         "2014",
-        "../imagenes/imagenesModelos/Moog subsecuent 37.jpg"
+        "../imagenes/imagenesModelos/Moog_Subsecuent_37.jpg"
     )
     
 ]
