c485dcd08bbf290b3e3ec49dca7b79ac78b33743
Nelson Adarmes <jhoeladarmes@gmail.com>
2022-11-04 09:29:32 -0300

    prueba corrección de errores en direcciones

--- listados/listadoGorras.js
+++ listados/listadoGorras.js
@@ -3,17 +3,17 @@ import {Gorra} from "../classesdeElementosBasicos/Gorra.js"
 let listadoGorras = [
     new Gorra ("Negro", 
     "Gorra ajustable Logo MOOG", 
-    "./imagenes/imagenesModelos/Moog_Gorra_Negra.jpg"
+    "../imagenes/imagenesModelos/Moog_Gorra_Negra.jpg"
     ),
 
     new Gorra ("Blanco", 
     "Gorra ajustable Synth", 
-    "./imagenes/imagenesModelos/Moog_Gorra_Blanca.jpg"
+    "../imagenes/imagenesModelos/Moog_Gorra_Blanca.jpg"
     ),
 
     new Gorra ("Azul", 
     "Gorra ajustable Star", 
-    "./imagenes/imagenesModelos/Moog_Gorra_Azul.jpg"
+    "../imagenes/imagenesModelos/Moog_Gorra_Azul.jpg"
     ),
 ]
 
